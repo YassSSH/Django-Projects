ff103ed6b60502a85de09cb25ac24d9a34897a55
YassSSH <yassine.regragui@mediaschool.me>
2021-11-27 16:26:32 +0100

    First page

--- Django-Project/mysite/leads/views.py
+++ Django-Project/mysite/leads/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def home_page(request):
+    return HttpResponse("""
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-sm bg-light">
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link 1</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="https://github.com/YassSSH">GitHub</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link 3</a>
+    </li>
+  </ul>
+</nav>
+<br>
+
+<div class="container-fluid">
+  <h3>DJANGO FIRST PAGE</h3>
+  <p>YassSSH</p>
+
+</div>
+
+</body>
+</html>
+    """)
